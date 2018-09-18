f25d1be8698ee66b98d02c93abb2bbd939461a12
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2018-09-18 17:54:11 +0530

    Link to the Openjfx docs repository

--- docs/js/ga-footer.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function () {
-    jQuery('a[href]').click(function () {
-        var match = jQuery(this).attr('href').match(/#\S+/);
-        ga('send', 'pageview', location.pathname + match[0]);
-    });
-});
